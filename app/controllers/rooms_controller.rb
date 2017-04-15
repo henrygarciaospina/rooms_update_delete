a26d344a7c21926de8585dafc1556e283e957992
osboxes.org <osboxes.henrygarcia0309@gmail.com>
2017-04-15 11:42:37 -0500

    Rooms options update and delete room

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,45 @@
+class RoomsController < ApplicationController
+  def index
+    @rooms = Room.all
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+      if @room.update(room_params)
+        redirect_to rooms_path
+      else
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    if @room.destroy
+        redirect_to rooms_path, :notice => "Room delete"
+    else
+        redirect_to rooms_path, :notice => "Room can't delete"
+    end
+  end
+
+  protected
+    def room_params
+      params.require(:room).permit(:title, :description, :beds, :guests, :image_url)
+    end
+end
